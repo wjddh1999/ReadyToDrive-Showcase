601a558ae77ceaee2d8f0d1e1eef2716a3731a7f
Kwon_j.o <109610334+wjddh1999@users.noreply.github.com>
2026-09-17 22:06:32 +0900

    chore: remove superseded RPM snippet

--- CodeSamples/Transmission/RpmTransition.cs
+++
@@ -1,8 +0,0 @@
-// Source: Assets/Scripts/CarState.cs
-// Excerpt: RPM response after an upshift
-// Review sample only; surrounding state branches are omitted.
-
-if (RPM <= 1000.0f)
-    RPMChange = false;
-
-RPM = Mathf.Lerp(RPM, 0.0f, Time.deltaTime * 2.0f);
